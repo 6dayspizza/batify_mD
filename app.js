b44bf7d748e4666806c6d74919cace35d41224a6
yesimverytired <kistlerr@oregonstate.edu>
2024-05-08 10:22:14 +0200

    trying to get server to start

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-// server.js
+/*
+    SETUP
+*/
+
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("views/public"));
+
+const { engine } = require("express-handlebars");
+var exphbs = require("express-handlebars");
+const hbs = exphbs.create({
+  partialsDir: "views/partials",
+  extname: ".hbs",
+});
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
 
 app.get('/', (req, res) => {
     res.send('hello');
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Microservice listening at http://localhost:${port}`);
 });
