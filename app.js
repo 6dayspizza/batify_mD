607573755ad68b58b8c22c08635fff9f25da0993
yesimverytired <kistlerr@oregonstate.edu>
2024-05-08 19:48:11 +0200

    enable CORS middleware

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const exphbs = require("express-handlebars");
 const fs = require('fs');
 const csv = require('csv-parser');
 
+
+// Enable CORS middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
 /*
     SETUP
 */
