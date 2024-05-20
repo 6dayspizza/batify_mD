f80c8bf6a7ce829dbbb3762ce8742aab6a51cf43
yesimverytired <kistlerr@oregonstate.edu>
2024-05-20 17:45:09 +0200

    adding volunteer file

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.engine(".hbs", hbs.engine);
 app.set("view engine", ".hbs");
 
 const filePathBats = 'files/batsoverview.csv';
+const filePathVolunteers = 'files/volunteers.csv';
 
 const readCSVFileBats = (filePathBats) => {
     return new Promise((resolve, reject) => {
@@ -45,11 +46,22 @@ const readCSVFileBats = (filePathBats) => {
     });
 };
 
+const readCSVFileVolunteers = (filePathVolunteers) => {
+    return new Promise((resolve, reject) => {
+        const lines = [];
+        fs.createReadStream(filePathVolunteers)
+            .pipe(csv())
+            .on('data', (data) => lines.push(data))
+            .on('end', () => resolve(lines))
+            .on('error', (error) => reject(error));
+    });
+};
+
 app.get('/', (req, res) => {
-    res.send('hello microserviceD');
+    res.send('hi microserviceD');
 });
 
-app.post('/', async (req, res) => {
+app.post('/species', async (req, res) => {
     const { species } = req.body;
     console.log('Received data:', species);
     
@@ -64,6 +76,44 @@ app.post('/', async (req, res) => {
         }
     } catch (error) {
         console.error('Error processing request:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+const getVolunteerWithLowestBats = async () => {
+    try {
+        const volunteers = await readCSVFileVolunteers(filePathVolunteers);
+        
+        // Find the volunteer with the lowest number of assigned bats
+        let minAssignedBats = Number.MAX_SAFE_INTEGER;
+        let selectedVolunteer = null;
+
+        volunteers.forEach(volunteer => {
+            const assignedBats = parseInt(volunteer['assignedbats']);
+            if (assignedBats < minAssignedBats) {
+                minAssignedBats = assignedBats;
+                selectedVolunteer = volunteer;
+            } else if (assignedBats === minAssignedBats) {
+                // Randomly select one of the volunteers with the lowest number of assigned bats
+                if (Math.random() < 0.5) {
+                    selectedVolunteer = volunteer;
+                }
+            }
+        });
+
+        return selectedVolunteer;
+    } catch (error) {
+        console.error('Error reading CSV file:', error);
+        throw error;
+    }
+};
+
+app.get('/volunteers', async (req, res) => {
+    try {
+        const volunteer = await getVolunteerWithLowestBats();
+        res.json(volunteer);
+    } catch (error) {
+        console.error('Error reading CSV file:', error);
         res.status(500).send('Internal Server Error');
     }
 });
